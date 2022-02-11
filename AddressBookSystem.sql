ec666d52e5a5dc5b12023a513c289a1b0dcf8b2d
Somanath Havinal <somusp75@gmail.com>
2022-02-11 19:38:48 +0530

    uc -4 editing contact bsaed on name
    editing contact

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -12,3 +12,20 @@ zip BigInt,
 PhoneNumber BigInt,
 Email varchar(200)
 )
+
+--UC-3 inserting values to the table
+Insert into Address_Book_Table(FirstName,LastName,Address,City,State,zip,PhoneNumber,Email) 
+values('somu','Havinal','Near Satti Road','Athani','Karnataka',591304,9731390823,'somusp75@gmail.com'),
+('chetan','Koparde','Main Road','Banahatti','Karnataka',587311,9980431245,'chetu65@gmail.com'),
+('gouri','shete','18 cross dadra','bangalore','chennai',587315,9740049061,'gourishete@gmail.com');
+
+select *from Address_Book_Table;
+
+--Uc-4 edititng contact using name
+Update Address_Book_Table
+set Email='ChetanKoparde@gmail.com'
+where FirstName='chetan'
+
+Update Address_Book_Table
+set Address='Main Road Rampur'
+where FirstName='gouri' and LastName='shete';
