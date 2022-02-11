4ebbbe1daddc243efc69af01dd6f565222fbc553
Somanath Havinal <somusp75@gmail.com>
2022-02-11 20:06:36 +0530

    uc-7 count book by city and state
    count size

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -11,8 +11,7 @@ State varchar(100),
 zip BigInt,
 PhoneNumber BigInt,
 Email varchar(200)
-
-
+)
 
 --UC-3 inserting values to the table
 Insert into Address_Book_Table(FirstName,LastName,Address,City,State,zip,PhoneNumber,Email) 
@@ -21,7 +20,6 @@ values('somu','Havinal','Near Satti Road','Athani','Karnataka',591304,9731390823
 ('gouri','shete','18 cross dadra','bangalore','chennai',587315,9740049061,'gourishete@gmail.com');
 
 select *from Address_Book_Table;
-
 
 --Uc-4 edititng contact using name
 Update Address_Book_Table
@@ -32,10 +30,18 @@ Update Address_Book_Table
 set Address='Main Road Rampur'
 where FirstName='gouri' and LastName='shete';
 
- --UC-5_DeletePersonByName
-
 delete 
 from Address_Book_Table
 where FirstName='chetan' and LastName='Koparde'
+
 select *from Address_Book_Table;
 
+select * from Address_Book_Table
+where City='bangalore' or State='chennai'
+
+---uc-7  Retrieve Count of Person belonging to a City or State----
+Insert into Address_Book_Table(FirstName,LastName,Address,City,State,zip,PhoneNumber,Email) 
+values('sirsu','Bhajantri','shivaji circle Jamkhandi','Belgavi','Karnataka',590006,9663424395,'sirsuba@gmail.com')
+select Count(*),state,City
+from Address_Book_Table
+Group by state,City
